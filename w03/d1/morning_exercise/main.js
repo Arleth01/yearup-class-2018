8f28b8301904d861782935a48390616b7ea3110d
Arleth Ontiveros <aontiveros@nyc.yearup.org>
2018-11-06 08:45:20 -0500

    html and css done

--- w03/d1/morning_exercise/main.js
+++ w03/d1/morning_exercise/main.js
@@ -1,1 +1,5 @@
 // Write a program that will `console.log` the numbers from 1 to 100. But for multiples of three `console.log` “Fizz” instead of the number and for the multiples of five `console.log` “Buzz”. For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+function FizzBuzz(i){
+  for var(let i=0; i<=100; i++)
+  if (i%5
+}
